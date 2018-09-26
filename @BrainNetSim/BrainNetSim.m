408775ac7afa3855c4f3c221c472f2f622081bb0
EBarzegaran <e.barzegaran@gmail.com>
2018-09-26 14:34:30 -0700

    add node and connection classes

--- @BrainNetSim/BrainNetSim.m
+++ @BrainNetSim/BrainNetSim.m
@@ -1,7 +1,7 @@
 classdef BrainNetSim
     
     properties
-        NodeNum
+        
         Nodes % a strcut array of nodes (NodeNum x 1), storing nodes information including
               % node.Type, Node.Freq, Node.TS %timeSeries
         Connections % a strut matrix of Connections information (NodeNum x NodeNum)including
@@ -13,11 +13,15 @@ classdef BrainNetSim
     end
     
     properties (Dependent)
-       
+       NodeNum
     end
     
     methods
-        
+        % dependent variable
+        function value = get.NodeNum(obj)
+            value = numel(obj.Nodes);
+        end 
+            
         %% network initialization
         function obj = BrainNetSim(NodeNum,SF,rlevel)
             % INPUT: 
@@ -26,29 +30,53 @@ classdef BrainNetSim
                     % rLevel: the amplitude of the node poles, value
                     % between 0 and 1, the closer to 1, the more
                     % oscillatory, default value .95
-            obj.NodeNum = NodeNum;
-            if ~exist('rlevel','var') || isempty(rlevel)
-                rlevel = 0.95; 
-            end
-            obj.Nodes = struct('Type',cell(NodeNum,1),'Freq',cell(NodeNum,1),'TS',cell(NodeNum,1),'r',num2cell(ones(NodeNum,1)*rlevel));
-            obj.Connections = struct('FiltType',cell(NodeNum),'FiltOrder',cell(NodeNum),'LF',cell(NodeNum),'HF',cell(NodeNum),'Gain',cell(NodeNum));
-            if ~exist('SF','var') || isempty(SF)
-                SF = 100;
-            end
+            
+            if ~exist('SF','var') || isempty(SF),SF = 100;end
+            if ~exist('rlevel','var') || isempty(rlevel),rlevel = 0.95;end
+            obj.Nodes = Node(NodeNum);
+            [obj.Nodes(:).r] = deal(rlevel);
+            obj.Connections = Connection(NodeNum);
             obj.SF = SF;
         end
         
+        %% Add a new node
+        function [obj,Node_Number] = AddNode (obj,Type,varargin)
+            % add an emoty node to the network
+            opt = ParseArgs(varargin,...
+                'TS'      ,'',...
+                'Freq'        ,[],...
+                'r'           ,obj.Nodes(end).r ...
+            );
+            
+            % update node and connection matrices
+            Node_Number = obj.NodeNum+1;
+            obj.Nodes(Node_Number) = Node(1);
+            obj.Connections(Node_Number,Node_Number) = Connection(1);
+            
+            obj.Nodes(Node_Number).Type = Type;
+            obj.Nodes(Node_Number).Freq = opt.Freq;
+            obj.Nodes(Node_Number).TS = opt.TS;
+            obj.Nodes(Node_Number).r = opt.r;
+            obj.Nodes(Node_Number).Number = Node_Number;
+        end
+        
         %% assign internal frequencies
-        function obj = AddNodeFreqs (obj,nodes, Freqs)
-            % syntax NetStrct.AddNodeFreqs(Freqs)
+        function obj = AddNodeFreqs (obj,nodes_num, Freqs)
+            % add internal frequency to AR nodes
+            % syntax NetStrct.AddNodeFreqs(nodes,Freqs)
                 % Freqs: is a cell array of internal frequencies of the
                         % nodes, each of the cell should contain one or two
                         % frequencies
-            %[obj.Nodes.Freq] = deal(Freqs{:});%, 4}; 
-            for i = 1:numel(nodes)
+            for i = 1:numel(nodes_num)
+                % (1) find the node
+                ind = find([obj.Nodes(:).Number] == nodes_num(i));
                 if ~isempty(Freqs{i})
-                    obj.Nodes(nodes(i)).Type = 'AR';
-                    obj.Nodes(nodes(i)).Freq = Freqs{i};
+                    if strcmp(obj.Nodes(nodes_num(ind)).Type,'AR')|| isempty(obj.Nodes(nodes_num(ind)).Type)
+                        obj.Nodes(nodes_num(ind)).Type = 'AR';
+                        obj.Nodes(nodes_num(ind)).Freq = Freqs{i};
+                    else
+                        error(['Unable to assign Freq to node' num2str(nodes_num(ind)) '. Check the type of node and change it to AR if necessary.'])
+                    end                    
                 end
             end
         end
@@ -57,7 +85,7 @@ classdef BrainNetSim
         function obj = AddConnection(obj, nodes,varargin)
             % array of 2x1, indicating the node connections: ATTENTION
             % order is important, it indicates the direction of connection
-            %
+            % nodes: [source sink] nodes
             opt = ParseArgs(varargin,...
                 'Type'      ,'',...
                 'LF'        ,[],...
@@ -65,7 +93,7 @@ classdef BrainNetSim
                 'Gain'      ,1, ...
                 'Order'     ,6 ...
                 );
-            
+            Connect1 = Connection();
             % check if the connection exist in the network
             if numel(nodes)~=2 || sum(~ismember(nodes, 1:obj.NodeNum))
                 error ('nodes should be a 2x1 vector indicating the node number of the network');
@@ -74,19 +102,22 @@ classdef BrainNetSim
             if ~ismember(opt.Type,{'low','high','bandpass'})
                 error ('No such filter is defined');
             else
-                obj.Connections(nodes(1),nodes(2)).FiltType = opt.Type;
+                Connect1.FiltType = opt.Type;
             end
-            obj.Connections(nodes(1),nodes(2)).LF = opt.LF;
-            obj.Connections(nodes(1),nodes(2)).HF = opt.HF;
-            obj.Connections(nodes(1),nodes(2)).Gain = opt.Gain; 
-            obj.Connections(nodes(1),nodes(2)).FiltOrder = opt.Order; 
+            Connect1.LF = opt.LF;
+            Connect1.HF = opt.HF;
+            Connect1.Gain = opt.Gain; 
+            Connect1.FiltOrder = opt.Order;
+            Connect1.Nodes = nodes;
+            
+            obj.Connections(nodes(1),nodes(2)) = Connect1;
         end
         
         %% AR calculation for linear models only
         function obj = GenerateARMatrix(obj)
             
             % First find the parameters including r(s) and noise level         
-            order = {obj.Connections.FiltOrder};order = cat(3,order{:});
+            order = [obj.Connections(:).FiltOrder];
             order = max(max(order)+1,4);
 
             if ~isempty(order) 
@@ -97,10 +128,12 @@ classdef BrainNetSim
             
             for n = 1: obj.NodeNum
                 f = obj.Nodes(n).Freq/obj.SF; % first frequency
-                [r,f]=parameter_search([2*pi*f],obj.Nodes(n).r,1);
-                [x,y] = pol2cart(f,r);Poles = x+1i*y;
-                [~,a] = zp2tf(0,[Poles conj(Poles)],1);
-                A(n,n,1:numel(a)-1)= -a(2:end);
+                if ~isempty(f)
+                    [r,f]=parameter_search([2*pi*f],obj.Nodes(n).r,1);
+                    [x,y] = pol2cart(f,r);Poles = x+1i*y;
+                    [~,a] = zp2tf(0,[Poles conj(Poles)],1);
+                    A(n,n,1:numel(a)-1)= -a(2:end);
+                end
             end
 
             % the second part is the Moving average (filters for connections)
@@ -109,7 +142,7 @@ classdef BrainNetSim
                     if ~isempty(obj.Connections(n1,n2).FiltType) && (n1~=n2) 
                         lf = obj.Connections(n1,n2).LF; hf = obj.Connections(n1,n2).HF;
                         b1 = fir1(obj.Connections(n1,n2).FiltOrder, [lf hf]./(obj.SF/2),obj.Connections(n1,n2).FiltType);
-                        A(n1,n2,1:numel(b1))= b1*obj.Connections(n1,n2).Gain/3;
+                        A(n1,n2,1:numel(b1))= b1*obj.Connections(n1,n2).Gain;
                     end
                 end
             end
@@ -128,7 +161,7 @@ classdef BrainNetSim
             for t = order+1:10000+NS
                 TS_temp = zeros(obj.NodeNum,1);
                 for ord = 1:order
-                    TS_temp = TS_temp +obj.ARMatrix(:,:,ord)*TS(:,t-ord)+randn(1,1).*alpha;
+                    TS_temp = TS_temp + obj.ARMatrix(:,:,ord)'*TS(:,t-ord)+randn(1,1).*alpha;
                 end
                 TS(:,t) = TS_temp;
             end
@@ -147,7 +180,8 @@ end
 %% Other functions
 function [r,f,Gain,F1,P1] = parameter_search(f,r0,P)
 % set the amplitude of the poles in a way that gain(f1)/gain(f2)=P
-% This is a recursive function
+% This needs to be checked: if the number of frequencies are bigger than 2,
+% then an optimization algorithm is required
 
 %initial values
 f = sort(f); % sort frequencies from low to high
